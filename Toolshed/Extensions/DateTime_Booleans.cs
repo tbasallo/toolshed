a0de0212f8c0e351224f3a60a68fc917672bd0e8
Tony Basallo <tony@whitespaces.design>
2021-09-09 21:57:26 -0400

    date isinrange

--- Toolshed/Extensions/DateTime_Booleans.cs
+++ Toolshed/Extensions/DateTime_Booleans.cs
@@ -24,7 +24,7 @@ namespace Toolshed
         }
 
         /// <summary>
-        /// Indicates 
+        /// Indicates
         /// </summary>
         /// <param name="date"></param>
         /// <returns></returns>
@@ -48,7 +48,7 @@ namespace Toolshed
         {
             return (date.Date == DateTime.Today.Date);
         }
-       
+
         /// <summary>
         /// Indicates whether the specified date is yesterday (.Date == DateTime.Today.AddDays(-1))
         /// </summary>
@@ -56,7 +56,7 @@ namespace Toolshed
         {
             return (date.Date == DateTime.Today.AddDays(-1).Date);
         }
-        
+
         /// <summary>
         /// Indicates whether the specified date is tomorrow (.Date == DateTime.Today.AddDays(1))
         /// </summary>
@@ -68,11 +68,11 @@ namespace Toolshed
         /// <summary>
         /// Indicates whether the specified dates are equal (no time component) (UTC)
         /// </summary>
-        private static bool IsDateEqualTo(this DateTime date, DateTime dateTwo)
+        public static bool IsDateEqualTo(this DateTime date, DateTime dateTwo)
         {
             return date.Date == dateTwo.Date;
         }
-        
+
         /// <summary>
         /// Indicates whether the specified date is an actual date, not null, not year 9999 and not less than year 0002
         /// </summary>
@@ -85,7 +85,7 @@ namespace Toolshed
 
             return false;
         }
-        
+
         /// <summary>
         /// Indicates whether the specified date is an actual date, not year 9999 and not less than year 0002 and not min or max date
         /// </summary>
@@ -97,6 +97,24 @@ namespace Toolshed
             }
 
             return true;
+        }
+
+        /// <summary>
+        /// Indicates whether this date is in range of the two specified dates, ONLY the date component (use IsInRange for complete date time) (>= Start, <= End). The Kind is not checked or converted
+        /// </summary>
+        /// <returns>Bool</returns>
+        public static bool IsDateInRange(this DateTime date, DateTime startDate, DateTime endDate)
+        {
+            return date.Date >= startDate.Date && date.Date <= endDate.Date;
+        }
+
+        /// <summary>
+        /// Indicates whether this date is in range of the two specified dates, including the time (>= Start, <= End). The Kind is not checked or converted
+        /// </summary>
+        /// <returns>Bool</returns>
+        public static bool IsInRange(this DateTime date, DateTime startDate, DateTime endDate)
+        {
+            return date >= startDate && date <= endDate;
         }
     }
 }
