48936d70606d2b019ab50c15d551c633fd8f6b48
Tony Basallo (TB4653) <tb4653@att.com>
2023-01-20 15:49:29 -0500

    fixed missing format

--- Toolshed/Extensions/Collections.cs
+++ Toolshed/Extensions/Collections.cs
@@ -69,7 +69,8 @@ namespace Toolshed
 
             if (collection.Count == 1)
             {
-                return collection.ElementAt(0).ToString();
+                if(string.IsNullOrWhiteSpace(format)) { return collection.ElementAt(0).ToString(); }
+                return string.Format(format, collection.ElementAt(0).ToString());
             }
 
             //no point in putting two empty spaces between the words
@@ -106,6 +107,65 @@ namespace Toolshed
 
             return sb.Remove(sb.Length - delimiter.Length, delimiter.Length).ToString().TrimEnd();
         }
+
+        /// <summary>
+        /// Returns the collection as a delimited string. For example, a collection of strings ["a", "b", "c", "d"] is returned as "a,b,c,d"
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection">The collection of items to delimit into a string</param>
+        /// <param name="delimiter">The delimiter to use to separate the collection items, Defaults to ","</param>
+        /// <param name="format">An optional format string that can be used to format each item while being delimited. Should use the following format: {0:p2}</param>
+        /// <param name="spaceBetweenDelimiter">Indicates whether a space should be inserted between the end of one delimiter and the start of the next item: "a,b,c" vs. "a, b, c"</param>
+        /// <param name="ignoreNullValues">If true, removes null values from the returned string. Defaults to false (i.e will return a,c)</param>
+        /// <returns>A string where the collection items are delimited using the specified delimiter</returns>
+        public static string ToDelimitedString(this ICollection<string> collection, string delimiter = ",", string format = null, bool spaceBetweenDelimiter = true, bool ignoreNullValues = false) where T : IComparable
+        {
+            if (collection.Count < 1)
+            {
+                return string.Empty;
+            }
+
+            if (collection.Count == 1)
+            {
+                if (string.IsNullOrWhiteSpace(format)) { return collection.ElementAt(0); }
+                return string.Format(format, collection.ElementAt(0));
+            }
+
+            //no point in putting two empty spaces between the words
+            if (delimiter == " " && spaceBetweenDelimiter)
+            {
+                spaceBetweenDelimiter = false;
+            }
+
+            var sb = new StringBuilder();
+            delimiter = spaceBetweenDelimiter ? delimiter + " " : delimiter;
+
+            if (string.IsNullOrWhiteSpace(format))
+            {
+                foreach (var item in collection)
+                {
+                    if (ignoreNullValues && item == null)
+                    {
+                        continue;
+                    }
+                    sb.AppendFormat("{0}{1}", item, delimiter);
+                }
+            }
+            else
+            {
+                foreach (var item in collection)
+                {
+                    if (ignoreNullValues && item == null)
+                    {
+                        continue;
+                    }
+                    sb.AppendFormat(format, item + delimiter);
+                }
+            }
+
+            return sb.Remove(sb.Length - delimiter.Length, delimiter.Length).ToString().TrimEnd();
+        }
+
 
         /// <summary>
         /// Returns the collection as a JavaScript array (e.g., ["val1","val2","val3"])
