4e0ae87a90491254a004ccf19717c1665a71114f
Tony Basallo <tony@whitespaces.design>
2025-04-17 17:26:28 -0400

    tweak Geo distance

--- Toolshed/Geo.cs
+++ Toolshed/Geo.cs
@@ -1,62 +1,46 @@
-﻿using System;
+﻿namespace Toolshed;
 
-namespace Toolshed
+public static class Geo
 {
-    public static class Geo
+    /// <summary>
+    /// Earth's radius in kilometers.
+    /// </summary>
+    public const double RADIUS_EARTH_KM = 6371.0;
+
+    /// <summary>
+    /// Earth's radius in miles.
+    /// </summary>
+    public const double RADIUS_EARTH_MI = 3958.8;
+
+    /// <summary>
+    /// Returns the distance between two coordinates in the specified unit.
+    /// </summary>
+    /// <param name="lat1"></param>
+    /// <param name="lon1"></param>
+    /// <param name="lat2"></param>
+    /// <param name="lon2"></param>
+    /// <param name="unit"></param>
+    /// <returns></returns>
+    public static double DistanceBetweenCoordinates(double lat1, double lon1, double lat2, double lon2, string unit = "M")
     {
-        /// <summary>
-        /// Returns the distance between two points
-        /// </summary>
-        /// <param name="Lat1"></param>
-        /// <param name="Long1"></param>
-        /// <param name="Lat2"></param>
-        /// <param name="Long2"></param>
-        /// <returns></returns>
-        public static double DistanceBetweenCoordinates(double Lat1, double Long1, double Lat2, double Long2)
-        {
-            /*
-                The Haversine formula according to Dr. Math.
-                http://mathforum.org/library/drmath/view/51879.html
-                
-                dlon = lon2 - lon1
-                dlat = lat2 - lat1
-                a = (sin(dlat/2))^2 + cos(lat1) * cos(lat2) * (sin(dlon/2))^2
-                c = 2 * atan2(sqrt(a), sqrt(1-a)) 
-                d = R * c
-                
-                Where
-                    * dlon is the change in longitude
-                    * dlat is the change in latitude
-                    * c is the great circle distance in Radians.
-                    * R is the radius of a spherical Earth.
-                    * The locations of the two points in 
-                        spherical coordinates (longitude and 
-                        latitude) are lon1,lat1 and lon2, lat2.
-            */
-            double dDistance = Double.MinValue;
-            double dLat1InRad = Lat1 * (Math.PI / 180.0);
-            double dLong1InRad = Long1 * (Math.PI / 180.0);
-            double dLat2InRad = Lat2 * (Math.PI / 180.0);
-            double dLong2InRad = Long2 * (Math.PI / 180.0);
 
-            double dLongitude = dLong2InRad - dLong1InRad;
-            double dLatitude = dLat2InRad - dLat1InRad;
 
-            // Intermediate result a.
-            double a = Math.Pow(Math.Sin(dLatitude / 2.0), 2.0) +
-                       Math.Cos(dLat1InRad) * Math.Cos(dLat2InRad) *
-                       Math.Pow(Math.Sin(dLongitude / 2.0), 2.0);
+        // Convert degrees to radians
+        double dLat = ToRadians(lat2 - lat1);
+        double dLon = ToRadians(lon2 - lon1);
 
-            // Intermediate result c (great circle distance in Radians).
-            double c = 2.0 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1.0 - a));
+        // Apply Haversine formula
+        double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                   Math.Cos(ToRadians(lat1)) * Math.Cos(ToRadians(lat2)) *
+                   Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
 
-            // Distance.
-            // const Double kEarthRadiusMiles = 3956.0;
-            const Double kEarthRadiusKms = 6376.5;
-            dDistance = kEarthRadiusKms * c;
+        double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
 
-            return dDistance;
-        }
-
+        // Return distance in the specified unit
+        return unit == "M" ? RADIUS_EARTH_MI * c : RADIUS_EARTH_KM * c;
+    }        
+    private static double ToRadians(double degrees)
+    {
+        return degrees * (Math.PI / 180);
     }
 }
