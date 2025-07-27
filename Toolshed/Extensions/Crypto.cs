d774aad94c2ea2858d83dd567f84b13de3064b36
tbasallo <tony@whitespaces.design>
2025-07-27 11:38:39 -0400

    Update Crypto class with new hashing methods
    Added `ToOneWayHash` for SHA256 Base64 encoding and replaced
    `OneWayHash` with `ToOneWayHashUrlSafe` for Base64Url encoding.
    Updated documentation for clarity on encoding differences.

--- Toolshed/Extensions/Crypto.cs
+++ Toolshed/Extensions/Crypto.cs
@@ -12,14 +12,27 @@ public static class Crypto
 
     /// <summary>
     /// Generates a one-way hash for the given string using the SHA256 algorithm.
+    /// The result is encoded using Base64 encoding (not the Base64Url) which may result in some differences in the output.
+    /// Meta requires non-Base64Url encoding for some of its APIs.
+    /// </summary>
+    /// <param name="input">The input string to hash.</param>
+    /// <returns>The one-way hash as a Base64Url-encoded string.</returns>
+    public static string ToOneWayHash(this string s)
+    {
+        return Convert.ToBase64String(SHA256.HashData(System.Text.Encoding.UTF8.GetBytes(s)));
+    }
+
+    /// <summary>
+    /// Generates a one-way hash for the given string using the SHA256 algorithm
     /// The result is encoded using Base64Url encoding.
     /// </summary>
     /// <param name="input">The input string to hash.</param>
     /// <returns>The one-way hash as a Base64Url-encoded string.</returns>
-    public static string OneWayHash(this string s)
+    public static string ToOneWayHashUrlSafe(this string s)
     {
         return Base64Url.EncodeToString(SHA256.HashData(System.Text.Encoding.UTF8.GetBytes(s)));
     }
+
 
     /// <summary>
     /// Generates an HMAC (Hash-based Message Authentication Code) for the given string using the specified key and hashing algorithm.
