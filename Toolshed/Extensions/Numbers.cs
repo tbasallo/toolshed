da169f2ea764d318d1613f4b6559eb3c6b0fc4c6
tbasallo <tony@whitespaces.design>
2026-02-10 15:40:30 -0500

    Add decimals versions

--- Toolshed/Extensions/Numbers.cs
+++ Toolshed/Extensions/Numbers.cs
@@ -75,6 +75,44 @@
         }
 
         /// <summary>
+        /// Returns the number rounded to the largest segment with the suffix of K,M,B for thousand, million or billion
+        /// </summary>
+        public static string ToSemanticValue(this decimal number)
+        {
+            if (number >= 1000000000)
+            {
+                return Math.Round(((number / 1000000000) * 10)) / 10 + "B";
+            }
+            if (number >= 1000000)
+            {
+                return Math.Round(((number / 1000000) * 10)) / 10 + "M";
+            }
+            if (number >= 1000)
+            {
+                return Math.Round(((number / 1000) * 10)) / 10 + "K";
+            }
+
+            if (number < 0)
+            {
+
+                if (number >= 1000000000)
+                {
+                    return Math.Round(((number / 1000000000) * 10)) / 10 * -1 + "B";
+                }
+                if (number >= 100000000)
+                {
+                    return Math.Round(((number / 100000000) * 10)) / 10 * -1 + "M";
+                }
+                if (number >= 1000)
+                {
+                    return Math.Round(((number / 1000) * 10)) / 10 * -1 + "K";
+                }
+            }
+
+            return Math.Round(number, 2).ToString();
+        }
+
+        /// <summary>
         /// Adds the ordinal suffix for st, nd, rd and th.
         /// </summary>
         public static string ToOrdinal(this int number)
@@ -114,6 +152,34 @@
             if (preTeen != 1)
             {
                 int lastDigit = (number % 10);
+                switch (lastDigit)
+                {
+                    case 1:
+                        suffix = "st";
+                        break;
+                    case 2:
+                        suffix = "nd";
+                        break;
+                    case 3:
+                        suffix = "rd";
+                        break;
+                }
+            }
+            return string.Format("{0}{1}", number, suffix);
+        }
+
+        /// <summary>
+        /// Adds the ordinal suffix for st, nd, rd and th.
+        /// </summary>
+        public static string ToOrdinal(this decimal number)
+        {
+            string suffix = "th";
+
+            // gets condition for 11, 12, 13
+            int preTeen = ((int)(number % 100) / 10);
+            if (preTeen != 1)
+            {
+                int lastDigit = ((int)number % 10);
                 switch (lastDigit)
                 {
                     case 1:
@@ -236,6 +302,41 @@
         }
 
         /// <summary>
+        /// Returns the file size as a descriptive string (12MB)
+        /// </summary>
+        public static string ToFileSize(this decimal bytes)
+        {
+            if (bytes == 0)
+            {
+                return "0 MB";
+            }
+
+            string filesize = "MB";
+            int divisor;
+
+            if (bytes >= 1073741824)
+            {
+                divisor = 1073741824;
+                filesize = "GB";
+            }
+            else if (bytes >= 1048576)
+            {
+                divisor = 1048576;
+            }
+            else if (bytes >= 1024)
+            {
+                divisor = 1024;
+                filesize = "KB";
+            }
+            else
+            {
+                return string.Format("{0:##.##} bytes", bytes);
+            }
+
+            return string.Format("{0:##.##} {1}", Arithmetic.Divide(bytes, divisor), filesize);
+        }
+
+        /// <summary>
         /// Formats the nullable value with the specified format if the number is not null,
         /// otherwise, returns the default value
         /// </summary>
@@ -274,6 +375,22 @@
         /// <param name="format">The format to use if the number has a value</param>
         /// <param name="defaultValue">The value to return if the number is null. Defaults to ""</param>
         public static string ToFormat(this long? d, string format, string defaultValue = "")
+        {
+            if (!d.HasValue)
+            {
+                return defaultValue;
+            }
+
+            return d.Value.ToString(format);
+        }
+
+        /// <summary>
+        /// Formats the nullable value with the specified format if the number is not null,
+        /// otherwise, returns the default value
+        /// </summary>
+        /// <param name="format">The format to use if the number has a value</param>
+        /// <param name="defaultValue">The value to return if the number is null. Defaults to ""</param>
+        public static string ToFormat(this decimal? d, string format, string defaultValue = "")
         {
             if (!d.HasValue)
             {
@@ -648,6 +765,56 @@
         /// Indicates whether this number is within the range, including the start and end of the range
         /// </summary>
         public static bool IsInRange(this byte value, byte startRange, byte endRange)
+        {
+            return (value.IsGreaterThanEqualTo(startRange) && value.IsLessThanEqualTo(endRange));
+        }
+
+        /// <summary>
+        /// Indicates whether this number is greater than the threshold provided
+        /// </summary>
+        public static bool IsGreaterThan(this decimal value, decimal threshold)
+        {
+            return value > threshold;
+        }
+        /// <summary>
+        /// Indicates whether this number is less than the threshold provided
+        /// </summary>
+        public static bool IsLessThan(this decimal value, decimal threshold)
+        {
+            return value < threshold;
+        }
+        /// <summary>
+        /// Indicates whether this number is less than or equal to the threshold provided
+        /// </summary>
+        public static bool IsLessThanEqualTo(this decimal value, decimal threshold)
+        {
+            return value <= threshold;
+        }
+        /// <summary>
+        /// Indicates whether this number is greater than or equal to the threshold provided
+        /// </summary>
+        public static bool IsGreaterThanEqualTo(this decimal value, decimal threshold)
+        {
+            return value >= threshold;
+        }
+        /// <summary>
+        /// Indicates whether this number is a postive number, greater than or equal to 0
+        /// </summary>
+        public static bool IsPositive(this decimal value)
+        {
+            return value >= 0;
+        }
+        /// <summary>
+        /// Indicates whether this number is a negative number, less than 0
+        /// </summary>
+        public static bool IsNegative(this decimal value)
+        {
+            return value < 0;
+        }
+        /// <summary>
+        /// Indicates whether this number is within the range, including the start and end of the range
+        /// </summary>
+        public static bool IsInRange(this decimal value, decimal startRange, decimal endRange)
         {
             return (value.IsGreaterThanEqualTo(startRange) && value.IsLessThanEqualTo(endRange));
         }
